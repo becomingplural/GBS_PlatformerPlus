f6d9b3120c214d77e7ef8e988782dc7654755e91
becomingplural <48106441+becomingplural@users.noreply.github.com>
2022-10-23 08:54:27 -0500

    Update platform.h
    Updated the header to (finally) include references to the global variables.

--- plugins/PlatformerPlus/engine/include/states/platform.h
+++ plugins/PlatformerPlus/engine/include/states/platform.h
@@ -16,8 +16,6 @@ void basic_x_col() BANKED;
 void basic_y_col(UBYTE drop_press) BANKED;
 void ground_reset() NONBANKED;
 
-
-
 extern WORD pl_vel_x;
 extern WORD pl_vel_y;
 extern WORD plat_min_vel;
@@ -31,5 +29,34 @@ extern WORD plat_jump_vel;
 extern WORD plat_grav;
 extern WORD plat_hold_grav;
 extern WORD plat_max_fall_vel;
+
+extern UBYTE plat_drop_through;   
+extern UBYTE plat_mp_group;        
+extern UBYTE plat_solid_group;    
+extern WORD plat_jump_min;        
+extern UBYTE plat_hold_jump_max; 
+extern UBYTE plat_extra_jumps;     
+extern WORD plat_jump_reduction;  
+extern UBYTE plat_coyote_max;     
+extern UBYTE plat_buffer_max;     
+extern UBYTE plat_wall_jump_max;   
+extern UBYTE plat_wall_slide;      
+extern WORD plat_wall_grav;       
+extern WORD plat_wall_kick;        
+extern UBYTE plat_float_input;     
+extern WORD plat_float_grav;      
+extern UBYTE plat_air_control;     
+extern WORD plat_air_dec;        
+extern UBYTE plat_run_type;      
+extern WORD plat_turn_acc;        
+extern UBYTE plat_run_boost;       
+extern UBYTE plat_dash;            
+extern UBYTE plat_dash_style;      
+extern UBYTE plat_dash_momentum;  
+extern UBYTE plat_dash_through;   
+extern WORD plat_dash_dist;       
+extern UBYTE plat_dash_frames;
+extern UBYTE plat_dash_ready_max; 
+
 
 #endif
