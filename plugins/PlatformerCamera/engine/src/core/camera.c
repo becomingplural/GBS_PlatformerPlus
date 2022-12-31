25e6bda64edd2b2202a9895cc9197d4f3b0fd7d4
becomingplural <48106441+becomingplural@users.noreply.github.com>
2022-12-31 00:08:42 -0600

    Rollback of Camera Lookahead
    Currently can't make this work consistently. I will keep trying to add it back in future versions.

--- plugins/PlatformerCamera/engine/src/core/camera.c
+++ plugins/PlatformerCamera/engine/src/core/camera.c
@@ -31,15 +31,16 @@ void camera_update() NONBANKED {
      I need to have an option for catchup speed. Otherwise the deadzone can get too wild.
 
     */
+
     if ((camera_settings & CAMERA_LOCK_X_FLAG)) {
         //Difference between player position and camera_x
         //The 8 in this formula is necessary for centering the camera, presumably because the sprite starts at x = 0
         WORD a_x = camera_x  - ((PLAYER.pos.x >> 4) + 8 + ((pl_vel_x >> 10)*plat_camera_lead));
 
         //Even out camera catchup to velocity but not when re-orienting.
-        if (pl_vel_x != 0 && plat_camera_lead != 0){
+        /*if (pl_vel_x != 0 && plat_camera_lead != 0){
                 a_x -= PLAYER.bounds.right;
-        }
+        }*/
         // Horizontal lock
         //Camera - Player = Negative when Player is to the right and camera is catching up by moving right
         if (plat_camera_follow & 1 && a_x < -camera_deadzone_x - camera_offset_x) {
